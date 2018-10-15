3127eb1ab62df88f3a2b7899844fd5cbd74c30f2
jimbowick <james.hanlon@iinet.net.au>
2018-10-15 12:40:43 +0800

    - Cleaned up dialogue/text for consistency and and continuancy - CenterPitScene now has multiple button text or main text options, defined by modes, depending on what other activities have been performed - Undead Knight/Barrack section. Button text now reflects the fact that after searching the barrack the first time, entering the barracks on future occasions, the player knows that entering the barracks starts an undead knight fight.

--- src/Launcher.kt
+++ src/Launcher.kt
@@ -115,28 +115,30 @@ object Hero:Fightable(
 }
 
 class WelcomeScene():Scene(
-    mainText =  "Welcome, my child. May your stay here be less painful than it is for most.",
-    button1Text =  "go into a dark pit",
-    button2Text = "refuse to endanger yourself",
-    nextScene1 = { CenterPitScene() },
+    mainText =  "You open your eyes and as the world comes into focus you become scared. You've never been to this place and have no memory of getting here. An alien plant bobbing nearby seems to contort and from it a voice emanates.\n" +
+            "'Welcome, my child. May your stay here be less painful than it is for most.' The voice seems close, but the plant quickly softens and returns to its sunshine languishing.\nYou sit in shock for a moment before realising you have only two choices. In the near distance is an excavated bit of earth surrounded by crude palisade wall.\n" +
+            "You can go check for any signs of civilised life, or you can sit here and waste away.",
+
+    button1Text =  "Explore the pit",
+    button2Text = "Sit in safety",
+    nextScene1 = { CenterPitScene(mode = 1) },
     nextScene2 = { ScaredScene(WelcomeScene()) },
     runOnShow = {
         Hero.hitCheckpoint(WelcomeScene())
     }
 )
 class ScaredScene(backScene: Scene):Scene(
-    mainText = "dont be scared, ${Hero.name}",
+    mainText = "You sit frozen, unsure of where you are or what to do. After some time, the temporary peace becomes discomfort. Stand, ${Hero.name}, and by fate be borne on wings of fire!",
     nextScene1 = { backScene },
-    button1Text = "okay..",
+    button1Text = "Stand and explore",
     numberOfButtons = 1
 )
 
 
 
-class CenterPitScene():Scene(
-    mainText = "you in pit" ,
-    button1Text = "fight like man",
-    button2Text = "avoid danger",
+class CenterPitScene(mode:Int):Scene(
+    button1Text = "Fight the beast",
+    button2Text = "Attempt to flee",
     nextScene1 = {
         FightScene(
                 enemy = Kobold(),
@@ -144,10 +146,17 @@ class CenterPitScene():Scene(
                 winScene = SidePitScene()
         )
     },
-    nextScene2 = {DeathScene()},
+    nextScene2 = {fleePit()},
     runOnShow = {
+        if(mode==1) it.mainText="Barely more than a bit of cleared earth, the pit shows fresh signs of life. A dying fire. The Burnt bones of a recent meal.\n" +
+                "You smell the wretched danger before you see it. A panting Kobold springs into action, appearing in front of you with eyes flashing at the chance for a fight." +
+                "Its scarred, stinking body and jagged blade tell you there's no chance for diplomacy."
+        if(mode==2) it.mainText="Wand in hand, you return to the Pit where the Kobolds reside. Do you wish to try your luck with another beast or will you sit a moment to inspect the wand?"
+        if(mode==3) it.mainText="You return to the den of the Kobolds empty handed after exploring the ruins for a time. Do you wish to fight a wretched Kobold or return to the ruins?"
+        if(mode==3) it.button2Text="Return to the altar"
+        if(mode==3) it.nextScene2= {FindWandScene()}
         if(Hero.wand){
-            it.button2Text = "use ur wand"
+            it.button2Text = "Draw your wand and examine the inscription"
             it.nextScene2 = {
                 MagicUpgradeScene(barracks(1))
             }
@@ -155,39 +164,49 @@ class CenterPitScene():Scene(
     }
 )
 
+class fleePit():Scene(
+        mainText = "You turn your back on the salivating wretch which seeks your life and move to escape. " +
+                "Just as you turn to run, a greedy Kobold waiting for the outcome of the battle steps in your way holding a thin blade.\n" +
+                "It slips in under your ribs, piercing your heart.",
+        button1Text = "Next",
+        nextScene1 = { DeathScene() }
+
+)
+
 class MagicUpgradeScene(backScene: Scene):Scene(
-        mainText =  "You examine the wand and feel yourself pulled into a strange realm. You find yourself in the realm of the Wandman." +
-                "He says he can heal you or empower your defence capabilities with his magic.",
+        mainText =  "You look closely upon the wand. The tiny etchings begin to glow with a thin silver light, moving across the surface of the wand. Coming from the centre of your belly, you feel an immense pull as if your entire being was drawn through the eye of needle." +
+                "You find yourself in the realm of the Wand.\n" +
+                "Without words, a humanoid form of pure light speaks to you through the mist. Towering above you, he lumbers slowly but you know, somehow, that he wishes you only kindness. You understand in an instant that he will heal your wounds and fortify your being.",
+        button1Text = "Accept the Wandman's defensive enchantment",
         nextScene1 =  {
             object :Scene(
                 nextScene1 =  { MagicUpgradeScene(backScene) },
-                button1Text = "Speak to the Wandman",
+                button1Text = "Communicate with the Entity",
                 numberOfButtons = 1,
                 runOnShow ={
                     if(Hero.armor < 6) {
                         Hero.armor++
-                        it.mainText = "You are imbued with defensive power, your armor value increases and you are fully " +
-                                "healed. Your armour is now ${Hero.armor}."
+                        it.mainText = "The entity encircles you with a whirling energy. Your skin and muscle harden in an instant. You feel as if your body has been tempered in a smith's fire abd your armour is now ${Hero.armor}."
                     }
                     else {
-                        it.mainText = "You have already reaped the benefits of this magical water or whatever. You return " +
-                                "to the last place"
+                        it.mainText = "You have already reaped the benefits of the fortifying enchantment."
                     }
 
                 }
 
             ){}
         },
+        button2Text = "Seek healing magics",
         nextScene2 =  {
             object :Scene(
                 nextScene1 =  { MagicUpgradeScene(backScene) },
-                button1Text = "Speak to the Wandman",
+                button1Text = "Communicate with the Entity",
                 numberOfButtons = 1,
                 runOnShow = {
                     if(Hero.currentHealth < Hero.maxHealth){
                         Hero.currentHealth = Hero.maxHealth
-                        it.mainText =  "You are imbued with vitality. Your currentHealth increases and you are fully healed. " +
-                                "Your currentHealth is now ${Hero.currentHealth}."
+                        it.mainText =  "You are imbued with vitality. Your health is replenished." +
+                                "Your health is now ${Hero.currentHealth}."
                     } else
                         it.mainText = "You are already completely healed. You return to the wand master."
 
@@ -195,19 +214,17 @@ class MagicUpgradeScene(backScene: Scene):Scene(
             ){}
         },
         nextScene3 =  { backScene },
-        button1Text = "Accept the Wandman's defensive enchantment",
-        button2Text = "Use the Wandman's healing magic",
-        button3Text = "Leave the realm of the Wandman",
+                button3Text = "Leave the realm of the Wand",
         numberOfButtons = 3
 )
 
 class DeathScene():Scene(
-    mainText =  "you just straight up die, son",
+    mainText =  "Your wounds are simply too great. You rest your head on the ground and your consciousness slips away.",
     nextScene1 =  {
         object :Scene(
-            mainText = "You awake to find your wounds healed, but your items are lost and your magical benefits removed. " +
+            mainText = "You awake to find your wounds healed, but your items are lost and your magical benefits removed.\n" +
                     "You will return to your last checkpoint",
-            button1Text = "okay",
+            button1Text = "Next",
             nextScene1 = { Hero.lastCheckpointScene },
             runOnShow = {
                 Hero.loadCheckpointHero()
@@ -217,22 +234,22 @@ class DeathScene():Scene(
             numberOfButtons = 1
         ){}
     },
-    button1Text = "oh holy mama, restart me",
+    button1Text = "Return to Checkpoint",
     numberOfButtons = 1
 )
 
 class FindWandScene():Scene(
-    button1Text =  "go back to the center of the pit",
+    button1Text =  "Return to the Kobold encampment.",
     nextScene1 = {
-        CenterPitScene()
+        CenterPitScene(mode = 2)
     },
     numberOfButtons = 1,
     runOnShow = {
         Hero.hitCheckpoint(FindWandScene())
         if(Hero.wand){
-            it.mainText = "you see the empty altar where the wand was"
+            it.mainText = "You see the empty altar where the wand once was."
         }else{
-            it.mainText = "you sneak around and find a wand!"
+            it.mainText = "You approach the burial mound and begin to climb. Sitting upon it is an altar, and upon that in turn an ancient wand. You quickly stash it amongst your cloak."
             Hero.wand = true
         }
     }
@@ -253,7 +270,9 @@ class barracks(mode:Int) : Scene(
         Hero.hitCheckpoint(barracks(mode))
         if(mode==1) it.mainText ="You are thrown out of the Wandman's realm and find yourself standing in front of an ancient barracks. Do you wish to search the building or return to the safety of the Wandman's realm?"
         if(mode==2) it.mainText ="You stand again before the barracks. Do you wish to challenge an Undead Knight or return to the safety of the Wandman's realm?"
+        if(mode==2) it.button1Text ="Fight an Undead Knight"
         if(mode==3) it.mainText ="You are thrown out of the Wandman's realm and find yourself standing again before the barracks. Do you wish to challenge an Undead Knight or return back to the Wandman's realm?"
+        if(mode==3) it.button1Text ="Fight an Undead Knight"
     }
 )
 
@@ -339,10 +358,10 @@ class GetHitScene(enemy: Fightable,responseScene: Scene):Scene(
 )
 
 class SidePitScene():Scene(
-    mainText = "You look around and see many small passages. behind you is the pit center",
-    button1Text = "go back center pit",
-    button2Text = "sneak around",
-    nextScene1 = {CenterPitScene()},
+    mainText = "Beyond the pit of Kobolds is an old burial mound, and upon it lies an altar.",
+    button1Text = "Return to the Kobolds pit",
+    button2Text = "Climb the mound and approach the altar.",
+    nextScene1 = {CenterPitScene(mode = 3)},
     nextScene2 = {FindWandScene()}
 )
 
